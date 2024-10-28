1d4a27c13e2be1d4803b3cfc29ae3f684bb8cd1e
Erick Andrade <127546150+alloerick@users.noreply.github.com>
2024-10-28 20:50:18 -0300

    feito

---
+++ dm06/assets/js/script.js
@@ -0,0 +1,39 @@
+const alunos = []
+
+document.getElementById('alunoForm').addEventListener('submit', function(event) {
+    event.preventDefault() // evita o envio do formulário
+
+    const name = document.getElementById('name').value
+    const age = parseInt(document.getElementById('age').value)
+
+    // verificando se aluno é maior de idade
+    const maiorIdade = age >= 18
+
+    // criando objeto aluno
+    const aluno = {
+        nome: name,
+        idade: age,
+        maiorIdade: maiorIdade
+    };
+
+    // Adicionando aluno a um array
+    alunos.push(aluno)
+
+    // dsiplay para exiber os alunos cadastrados na array
+    displayAlunos()
+
+    // limpa os campos
+    document.getElementById('alunoForm').reset()
+});
+
+// função que exibi os alunos do array
+function displayAlunos() {
+    const alunoList = document.getElementById('alunoList')
+    alunoList.innerHTML = ''
+
+    alunos.forEach(aluno => {
+        const li = document.createElement('li')
+        li.textContent = `Nome: ${aluno.nome}, Idade: ${aluno.idade}, ${aluno.maiorIdade ? 'Maior de Idade' : 'Menor de Idade'}`
+        alunoList.appendChild(li)
+    })
+}
